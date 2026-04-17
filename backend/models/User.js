00b972fc3fb246b1127b2ecaec88ddd3646b7b23
VamshiKrishnaSiribommala <vamshikrishnasiribommala@gmail.com>
2026-04-17 15:47:26 +0530

    comment10

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,27 +1,20 @@
 const mongoose= require("mongoose")
-const userSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        unique:true,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    mobile:{
-        type:Number,
-        required:true
-    },
-    role:{
-        type:String,
-        enum:["user","admin"],
-        default:"user"
-    }
-},{timestamps:true})
 
-module.exports=mongoose.model("User",userSchema)
+const cartSchema= mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    item:[{
+        productId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Product"
+        },
+        quantity:{
+            type:Number,
+            default:1
+        }
+    }]
+})
+
+module.exports=mongoose.model("Cart",cartSchema)
